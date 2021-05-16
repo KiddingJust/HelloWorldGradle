601146d75069a7cada4153ce59dde0702684fdea
Kidding Just <rlarkdlr@gmail.com>
2021-05-16 21:05:24 +0900

    hello world git

---
+++ src/test/java/org/gaiga/HelloWorldGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gaiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
